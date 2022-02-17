b8726f4afda27c054ca9bcd40cd3abb6ea765764
mijail <mijail.iso@ciencias.unam.mx>
2022-02-17 11:30:08 -0600

    testing mijail final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test module 5'));
+app.get('/', (req, res) => res.send('Mi test mijail'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
